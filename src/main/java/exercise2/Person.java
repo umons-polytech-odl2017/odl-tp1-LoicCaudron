8cebdb7177e6560400d898b738d801fe971a8c28
LoicCaudron <Loic.CAUDRON@student.umons.ac.be>
2017-10-20 14:25:52 +0200

    WIP

--- src/main/java/exercise2/Person.java
+++ src/main/java/exercise2/Person.java
@@ -1,4 +1,30 @@
 package exercise2;
 
 class Person {
+	private static int populationsize;
+	private static long totalage = 0;
+	private int age;
+
+	public Person(int age){
+
+		this.age=age;
+		populationsize++;
+		totalage += age;
+
+	}
+
+	public static int getPopulationSize()  {
+
+		return populationsize;
+	}
+
+	public static float computePopulationAverage(){
+
+		return ((float)totalAge)/populationsize;
+	}
+
+	public static void resetPopulation(){
+
+		populationsize=0;
+	}
 }
--- src/main/java/exercise3/ConvexPolygon.java
+++ src/main/java/exercise3/ConvexPolygon.java
@@ -1,4 +1,5 @@
 package exercise3;
 
 public class ConvexPolygon {
+
 }
